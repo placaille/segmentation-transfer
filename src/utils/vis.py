4fef6a3bc08cc2e97e54b4e65b08e01d0233095d
Sam <samuel_lavoie@hotmail.com>
2018-11-22 16:12:35 -0500

    Add vis file

---
+++ src/utils/vis.py
@@ -0,0 +1,57 @@
+import numpy as np
+import visdom
+from common.utils import get_args_dict
+
+
+def setup(args):
+    visualiser = visdom.Visdom(args.server, port=args.port, env=args.exp_name, use_incoming_socket=False)
+    if not args.reload:
+        visualiser.delete_env(args.exp_name)
+    visualise_text(visualiser, str(get_args_dict(args)))
+    return visualiser
+
+
+def visualise_image(visualiser, images, title=None, iteration=0, env=None):
+    nrow = int(len(images)**(1/2))
+    win = 'images%s%s' % (visualiser.env, iteration)
+    visualiser.images(images, nrow=nrow, win=win, opts={'title': title}, env=env)
+
+
+def visualise_text(visualiser, text, iteration=0, env=None):
+    visualiser.text(text, win='text%s' % iteration, env=env)
+
+
+def visualise_plot(visualiser, X, data, title=None, legend=None, iteration=0, update=None, env=None, name=None):
+    # FIXME only work for 1 D data.
+    if legend is None:
+        legend = np.arange(len(np.array(data)))
+    win = 'plot%s%s' % (visualiser.env, iteration)
+    opts = {'title': title, 'legend': legend or range(len(data))}
+    dataY = np.array([data[-1]]) if update == 'append' else data
+    err = visualiser.line(X=X, Y=dataY, win=win, opts=opts, update=update, env=env, name=name)
+    if win != err:
+        visualiser.line(X=X, Y=data, win=win, opts=opts, env=env, name=name)
+
+
+def visualise_video(visualiser, videofile, title=None, iteration=0):
+    win = 'video%s%s' % (visualiser.env, iteration)
+    opts = {'fps': 1, 'title': title}
+    visualiser.video(videofile=videofile, win=win, opts=opts)
+
+
+def visualise_bar(visualiser, bar, Y, title=None, iteration=0, env=None):
+    win = 'bar%s%s' % (visualiser.env, iteration)
+    opts = {'title': title}
+    visualiser.bar(X=bar, Y=Y, win=win, opts=opts, env=env)
+
+
+def visualise_box(visualiser, X, Y, title='', iteration=0, env=None):
+    win = 'bar%s%s' % (visualiser.env, iteration)
+    opts = {'title': title, 'legend': Y}
+    visualiser.boxplot(X=X, win=win, opts=opts, env=env)
+
+
+def remove_point(visualiser, title, iteration, env=None):
+    win = 'plot%s%s' % (visualiser.env, iteration)
+    opts = {'title': title, 'legend': legend or range(len(data))}
+    visualiser.line(X=None, Y=None, win=win, opts=opts, update='remove', env=env, name=iteration)
