ca1368f7b7ea76b82a54e54d2702754eb71864d7
Sam <samuel_lavoie@hotmail.com>
2018-11-23 10:45:58 -0500

    H5py dataset

---
+++ src/utils/loader.py
@@ -0,0 +1,14 @@
+import torch
+import h5py
+
+class H5PYDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset, classes, transform=None):
+        self.tensor = dataset
+        self.classes = classes
+
+    def __getitem__(self, index):
+        input = self.transform(self.tensor[index]) if self.transform is not None else self.tensor[index]
+        return input, self.classes[index]
+
+    def __len__(self):
+        return len(self.tensor)
