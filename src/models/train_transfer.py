6241668059bb5575d916e539749c926d09c35d51
Philippe Lacaille <philippe.lacaille@gmail.com>
2018-12-11 17:22:00 -0500

    Add pixel-wise objective to keep content from source image

--- src/models/train_transfer.py
+++ src/models/train_transfer.py
@@ -45,9 +45,12 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 @click.option('--batch-per-eval', type=int, default=500, help='default 500')
 @click.option('--batch-per-save', type=int, default=5000, help='default 5000')
 @click.option('--max-num-batch', type=int, default=250000, help='default 250000')
-def main(data_sim_dir, data_real_dir, data_label_dir, save_dir, visdom_dir, batch_size,
-         config_file, discr_model_name, gen_model_name, early_stop_patience, max_num_batch,
-         server, port, reload, run_name, batch_per_eval, batch_per_save, seg_model_path, seg_model_name):
+@click.option('--content-weight', type=float, default=0.0001)
+def main(data_sim_dir, data_real_dir, data_label_dir, save_dir, visdom_dir,
+         batch_size, config_file, discr_model_name, gen_model_name,
+         early_stop_patience, max_num_batch, server, port, reload, run_name,
+         batch_per_eval, batch_per_save, seg_model_path, seg_model_name,
+         content_weight):
 
     print('Loading data..')
     num_classes = 4
@@ -84,6 +87,7 @@ def main(data_sim_dir, data_real_dir, data_label_dir, save_dir, visdom_dir, batc
         model_seg.load(seg_model_path)
 
     obj_adv = nn.BCELoss()
+    content_obj = nn.MSELoss(reduction='none')
     label_true = 1
     label_fake = 0
 
@@ -101,17 +105,19 @@ def main(data_sim_dir, data_real_dir, data_label_dir, save_dir, visdom_dir, batc
     for eval_count in count(start=1):
 
         batch_since_eval = 0
-        loss_discr_true = 0
-        loss_discr_fake = 0
-        loss_gen = 0
+        loss_discr_true_sum = 0
+        loss_discr_fake_sum = 0
+        loss_gen_style_sum = 0
+        loss_gen_content_sum = 0
         model_gen.train()
         model_discr.train()
         start = timer()
 
         while batch_since_eval < batch_per_eval:
 
-            batch_real = next(real_data)
-            batch_sim, _ = next(sim_data)
+            batch_real = next(real_data).to(device)
+            batch_sim = next(sim_data)
+            batch_sim = batch_sim[0].to(device)
             assert batch_sim.shape[0] == batch_real.shape[0]
             b_size = batch_sim.shape[0]
             batch_count += 1
@@ -119,21 +125,17 @@ def main(data_sim_dir, data_real_dir, data_label_dir, save_dir, visdom_dir, batc
             loss_d = 0
             optim_discr.zero_grad()
             # train discriminator on true data (logD(x))
-            scores_true = model_discr(batch_sim.to(device))
+            scores_true = model_discr(batch_sim)
             labels = torch.full((b_size, ), label_true).to(device)
             loss_d_true = obj_adv(scores_true.view(-1), labels.to(device))
             loss_d_true.backward()
-            loss_d += loss_d_true.item()
-            loss_discr_true += loss_d_true.item()
 
             # train discriminator on fake data (log(1-D(G(x)))
-            batch_fake = model_gen(batch_real.to(device))
+            batch_fake = model_gen(batch_real)
             scores_fake = model_discr(batch_fake.detach())
             labels.fill_(label_fake)
             loss_d_fake = obj_adv(scores_fake.view(-1), labels)
             loss_d_fake.backward()
-            loss_d += loss_d_fake.item()
-            loss_discr_fake += loss_d_fake.item()
 
             optim_discr.step()
 
@@ -142,25 +144,33 @@ def main(data_sim_dir, data_real_dir, data_label_dir, save_dir, visdom_dir, batc
             # train generator
             scores_fake = model_discr(batch_fake)
             labels.fill_(label_true)
-            loss_g_fake = obj_adv(scores_fake.view(-1), labels)
-            loss_g_fake.backward()
-            loss_g += loss_g_fake.item()
-            loss_gen += loss_g_fake.item()
+            loss_g_style = obj_adv(scores_fake.view(-1), labels)
+            loss_g_tot = loss_g_style
+            if content_weight > 0:
+                loss_g_content = content_weight * content_obj(batch_fake, batch_real).sum((1, 2, 3)).mean()
+                loss_g_tot += loss_g_content
+            loss_g_tot.backward()
 
             optim_gen.step()
 
-            loss_tot = loss_d + loss_g
+            loss_discr_fake_sum += loss_d_fake.item()
+            loss_discr_true_sum += loss_d_true.item()
+            loss_gen_style_sum += loss_g_style.item()
+            loss_gen_content_sum += loss_g_content.item()
+            loss_tot = loss_d_fake.item() + loss_d_true.item() + loss_g_style.item() + loss_g_content.item()
+
             X, data = np.array([batch_count]), np.array([loss_tot])
-            visualiser.plot(X, data, title='Loss per batch', legend=['Loss'], iteration=2, update='append')
+            visualiser.plot(X, data, title='Loss per batch', legend=['Loss total'], iteration=2, update='append')
 
             batch_since_eval += 1
 
         # DO EVAL
         torch.cuda.empty_cache()
         eval_count += 1
-        results['loss_gen'] = np.divide(loss_gen, batch_since_eval)
-        results['loss_discr_fake'] = np.divide(loss_discr_fake, batch_since_eval)
-        results['loss_discr_true'] = np.divide(loss_discr_true, batch_since_eval)
+        results['loss_gen_style'] = np.divide(loss_gen_style_sum, batch_since_eval)
+        results['loss_gen_content'] = np.divide(loss_gen_content_sum, batch_since_eval)
+        results['loss_discr_fake'] = np.divide(loss_discr_fake_sum, batch_since_eval)
+        results['loss_discr_true'] = np.divide(loss_discr_true_sum, batch_since_eval)
 
         if seg_model_path:
             results.update(evaluate_transfer(model_seg, model_gen, real_data.get_valid_iterator(), device))
@@ -178,24 +188,24 @@ def main(data_sim_dir, data_real_dir, data_label_dir, save_dir, visdom_dir, batc
 
 def log_and_viz_results(results, batch_id, eval_count, visualiser, start, save_dir):
 
-    print('batch {:6d} - loss D(fake)/D(true)/G {:7.4f}/{:7.4f}/{:7.4f} - valid acc {:7.4f} - {:4.1f} secs' \
+    print('batch {:6d} - loss D/G {:7.4f}/{:7.4f} - {:4.1f} secs' \
           .format(
               batch_id,
-              results['loss_discr_fake'],
-              results['loss_discr_true'],
-              results['loss_gen'],
-              results['accuracy'],
+              results['loss_discr_fake'] + results['loss_discr_true'],
+              results['loss_gen_style'] + results['loss_gen_content'],
               timer()-start
           ))
 
     X = np.array([eval_count])
     data_d_fake = np.array([results['loss_discr_fake']])
     data_d_true = np.array([results['loss_discr_true']])
-    data_gen = np.array([results['loss_gen']])
+    data_gen_style = np.array([results['loss_gen_style']])
+    data_gen_content = np.array([results['loss_gen_content']])
 
-    visualiser.plot(X, data_d_fake, title='Discriminator fake loss', legend=['Loss'], iteration=0, update='append')
-    visualiser.plot(X, data_d_true, title='Discriminator true loss', legend=['Loss'], iteration=1, update='append')
-    visualiser.plot(X, data_gen, title='Generator loss', legend=['Loss'], iteration=3, update='append')
+    visualiser.plot(X, data_d_fake, title='Discriminator loss (fake)', legend=['Loss'], iteration=0, update='append')
+    visualiser.plot(X, data_d_true, title='Discriminator loss (true)', legend=['Loss'], iteration=1, update='append')
+    visualiser.plot(X, data_gen_style, title='Generator loss (style)', legend=['Loss'], iteration=3, update='append')
+    visualiser.plot(X, data_gen_content, title='Generator loss (content)', legend=['Loss'], iteration=5, update='append')
 
     visualiser.image(results['images'], title='Source image', iteration=0)
     visualiser.image(results['transformed'], title='Transformed image', iteration=1)
